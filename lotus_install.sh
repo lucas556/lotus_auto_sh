4018c3be46607ebe5780034ac6b6bc0783660892
lucas556 <56923561@qq.com>
2020-08-01 01:39:13 +0800

    Update lotus_install.sh

--- lotus_install.sh
+++ lotus_install.sh
@@ -28,28 +28,48 @@ sudo apt -y install golang-go mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq 
 
 # rustc
 mkdir -p $HOME/.cargo/
-cat > $HOME/.cargo/ <<EOF
+cat > $HOME/.cargo/config <<EOF
 [source.crates-io]
 replace-with = 'tuna'
 
 [source.tuna]
 registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"
+EOF
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -sSf | sh /dev/stdin "-y"
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
 echo 'export GOPROXY=https://goproxy.cn' >> ~/.bashrc
-EOF
-
 source ~/.bashrc
-# lotus
 
+# lotus
 git clone -b ntwk-calibration https://github.com/filecoin-project/lotus.git /lotus
 cd /lotus
-
 # 编译
 env RUSTFLAGS="-C target-cpu=native -g" FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1 FIL_PROOFS_USE_GPU_TREE_BUILDER=1 FFI_BUILD_FROM_SOURCE=1 make clean all
 make install
+
 # nfs mount
+mkdir -p /filecoin-proof-parameters
+sudo mount -t nfs -o vers=4 192.168.81.100:/ filecoin-proof-parameters
+echo '192.168.81.100:/ /filecoin-proof-parameters nfs   defaults,timeo=900,retrans=5,_netdev  0 0' >> /etc/fstab
+
+#lotus 环境变量
+mkdir -p /lotus_daemon
+cat > ~/.bashrc <<EOF
+export LOTUS_PATH=/lotus_daemon/
+export LOTUS_STORAGE_PATH=/lotusstorage/
+export FIL_PROOFS_PARAMETER_CACHE=/filecoin-proof-parameters/
+export WORKER_PATH=/lotusworker/
+export FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1
+export FIL_PROOFS_USE_GPU_TREE_BUILDER=1
+TOKEN=
+API=
+export MINER_API_INFO=$TOKEN:$API
+EOF
+
+cp /filecoin-proof-parameters/lotus_daemon/api /lotus_daemon
+cp /filecoin-proof-parameters/lotus_daemon/token /lotus_daemon
+
 # supervisor
 
 
