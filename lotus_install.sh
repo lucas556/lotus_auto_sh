af99cf9eca9a3e305d5cd34391bb1322a7b0eebf
lucas556 <56923561@qq.com>
2020-07-31 22:17:35 +0800

    Update lotus_install.sh

--- lotus_install.sh
+++ lotus_install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-
+# 更换中科大源
+sudo cp sources.list /etc/apt/sources.list
+# golang
+sudo add-apt-repository ppa:longsleep/golang-backports
 apt-get update
+
+# lotus依赖
+sudo apt install golang-go mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl
+
+# rustc
+mkdir -p ~/.cargo/
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
