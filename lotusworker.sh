33a54e6a8416ae33e869f3fb7197012b83aa3d80
lucas556 <56923561@qq.com>
2020-08-08 01:37:19 +0800

    Update lotusworker.sh

--- lotusworker.sh
+++ lotusworker.sh
@@ -4,7 +4,7 @@ function replace_source()
 {
   # 更换中科大源
   sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
-  cat > /etc/apt/sources.list << EOF
+  cat > /etc/apt/sources.list << 'EOF'
   ##中科大源
   deb https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
   deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
@@ -31,7 +31,7 @@ function install_environment()
 
   # rustc
   mkdir -p $HOME/.cargo/
-  cat > $HOME/.cargo/config << EOF
+  cat > $HOME/.cargo/config << 'EOF'
   [source.crates-io]
   replace-with = 'tuna'
   [source.tuna]
