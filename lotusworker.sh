f8b1a75ef6b61682fbad8daf86b2af30c1546bb9
lucas556 <56923561@qq.com>
2020-08-08 14:50:28 +0800

    Update lotusworker.sh

--- lotusworker.sh
+++ lotusworker.sh
@@ -50,7 +50,7 @@ function install_lotus()
   cd /
   git clone -b ntwk-calibration-8.8.0 https://github.com.cnpmjs.org/filecoin-project/lotus.git
   cd /lotus
-  git checkout -b ntwk-calibration-8.1.0
+  git checkout -b ntwk-calibration-8.8.0
   # 编译
   env RUSTFLAGS="-C target-cpu=native -g" FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1 FIL_PROOFS_USE_GPU_TREE_BUILDER=1 FFI_BUILD_FROM_SOURCE=1 make clean all
   make install
