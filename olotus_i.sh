c2453308aa6a80fbc2fa52a5e524fe69ef50671f
lucas556 <56923561@qq.com>
2020-12-07 12:11:57 +0800

    Create olotus_i.sh

---
+++ olotus_i.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "更新源开始"
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+cat > /etc/apt/sources.list << EOF
+##阿里源
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+EOF
+apt-get update
+
+echo "更新源成功"
+
+echo "安装依赖"
+
+sudo apt -y install mesa-opencl-icd ocl-icd-opencl-dev gcc clang git bzr jq pkg-config llvm curl nfs-common supervisor hwloc libhwloc-dev ntpdate ubuntu-drivers-common
+echo "安装成功"
+
+echo "部署环境"
+# time adjust
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ntpdate ntp.aliyun.com
+
+# install ulimit
+ulimit -n 1048576
+sed -i "/nofile/d" /etc/security/limits.conf
+echo "* hard nofile 1048576" >> /etc/security/limits.conf
+echo "* soft nofile 1048576" >> /etc/security/limits.conf
+echo "root hard nofile 1048576" >> /etc/security/limits.conf
+echo "root soft nofile 1048576" >> /etc/security/limits.conf
+
+echo "设置SWAP"
+sudo dd if=/dev/zero of=/swap bs=1G count=64
+sudo chmod 600 /swap
+sudo mkswap /swap
+sudo swapon /swap
+echo "swap修改完成,在/etc/fstab修改开机启动并重启"
+echo "设置SWAP"
