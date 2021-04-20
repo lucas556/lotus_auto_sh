e48174fb4760a7d21c3a0c31d1b028274ff2ba9b
lucas556 <56923561@qq.com>
2021-04-20 23:50:40 +0800

    Update worker.sh

--- worker.sh
+++ worker.sh
@@ -69,3 +69,5 @@ echo "vm.dirty_expire_centisecs=100" >> /etc/sysctl.conf
 // 报错 : lotus-miner: error while loading shared libraries: libhwloc.so.5: cannot open shared object file: No such file or directory
 // sudo ln -s /usr/lib/x86_64-linux-gnu/libhwloc.so /usr/lib/libhwloc.so.5
 
+// 删除ret-wait任务
+// lotus-miner sealing abort $id
